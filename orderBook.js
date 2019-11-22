98efec8c1f09869baebb1a07a9add00fea1771f1
Keefe Laporte <keefelaporte2019@gmail.com.com>
2019-11-21 19:04:11 -0500

    order up

--- orderBook.js
+++ orderBook.js
@@ -1,5 +1,32 @@
 function reconcileOrder(existingBook, incomingOrder) {
-  throw new Error('Not Implemented Yet')
+  existingBook.push(incomingOrder)
+ const totalOrders = existingBook.concat([incomingOrder])
+ let btcOrder = totalOrders[0]
+ let btcOrder2 = totalOrders[1]
+ if (btcOrder.type !== btcOrder2.type &&
+  btcOrder.price === btcOrder2.price &&
+  btcOrder.quantity === btcOrder2.quantity) {
+    return []
+  }
+  if (btcOrder.type !== btcOrder2.type &&
+    btcOrder.quantity !== btcOrder2.quantity &&
+    btcOrder.price === btcOrder2.price) {
+    if (btcOrder.quantity - btcOrder2.quantity > 0) {
+      return [{
+        type: 'buy',
+        quantity: btcOrder.quantity - btcOrder2.quantity,
+        price: btcOrder.price,
+      }]
+    }
+      else if (btcOrder2.quantity - btcOrder.quantity > 0) {
+        return [{
+          type: 'sell',
+          quantity: btcOrder2.quantity - btcOrder.quantity,
+          price: btcOrder.price,
+        }]
+    }
+  }
+ return existingBook
 }
-
 module.exports = reconcileOrder
+
